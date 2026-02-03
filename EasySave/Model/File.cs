9b959799c9803c2c9a5a61b51ff69e9646960913
Binz <melihgencler2019@gmail.com>
2026-02-03 09:08:07 +0100

    Correction du model File et ajout de l'interface ISave

---
+++ EasySave/Model/File.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.Model
+{
+    internal class File
+    {
+        public int fileSize { get; set; }
+        public int progress { get; set; }
+        public int nbFilesLeftToDo { get; set; }
+        public int totalFilesSize { get; set; }
+        public int totalFilesToCopy { get; set; }
+        public string? sourceFilePath { get; set; }
+        public string? targetFilePath { get; set; }
+        public string? state { get; set; }
+    }
+}
---
+++ EasySave/Model/ISave.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.Model
+{
+    internal interface ISave
+    {
+        string name { get; set; }
+        string sourceFilePath { get; set; }
+        string targetFilePath { get; set; }
+    }
+}
