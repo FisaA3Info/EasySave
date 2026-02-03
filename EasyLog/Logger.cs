81d366f747b7b39781d5c1a7e9fb0d66d92e7c8d
fvancamp5 <theotimevancamp@gmail.com>
2026-02-03 17:31:48 +0100

    feat(easy-log): Creating Log Library and its two main classes

---
+++ EasyLog/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyLog
+{
+    internal class Logger
+    {
+        public string LogDirectory { get; set; }
+
+        Logger () { }
+
+        public static void Log(LogEntry entry)
+        {
+
+        }
+
+        private static string GetDailyLogPath()
+        {
+            return "";
+        }
+    }
+}
