b0d4be6a7e52095201c829fed35b95a37455e3cd
Floraaa <150044772+LonelyBadeline@users.noreply.github.com>
2026-02-04 09:34:04 +0100

    feat(model): WIP:StateTracker & IStateObserver implementation

--- EasySave/Model/StateTracker.cs
+++ EasySave/Model/StateTracker.cs
@@ -7,11 +7,53 @@ namespace EasySave.Model
     internal class StateTracker
     {
         public string FilePath { get; set; }
-        public List<StateEntry> States { get; set; }
+        public List<StateEntry> States { get; private set; }
+        private List<IStateObserver> observers;
 
-        public void UpdateState(StateEntry entry)
+        public StateTracker(string filePath ="state.json")
         {
-            
+            FilePath = filePath;
+            States = new List<StateEntry>();
+            observers = new List<IStateObserver>();
+            LoadState();
+        }
+
+        //Observer Management Section
+
+        public void AttachObserver(IStateObserver observer)
+        {
+            if (!observer.Contains(observer))
+            {
+                observer.Add(observer);
+            }
+        }
+           
+        public void DetachObserver(IStateObserver observer)
+        {
+            observer.Remove(observer);
+        }
+
+        private void NotifyObservers(StateEntry entry)
+        {
+            foreach (var observer in observers)
+            {
+                observer.OnStateChanged(entry);
+            }
+        }
+
+        // StateTracker base method 
+        public void UpdateState(StateEntry entry)
+        
+        {
+            //Stock the result of FirstOrDefault on States which return the first value that statify the search or return null
+            var existantState = States.FirstOrDefault(search => search.JobName == entry.JobName,null);
+            if (existing != null)
+            {
+                States.Remove(existing);
+            }
+            States.Add(entry);
+            SaveState();
+            NotifyObservers(entry);
         }
 
         public void SaveState()
