2e760625db051ed8d3b67d377d778cb03c3009ab
Binz <melihgencler2019@gmail.com>
2026-02-05 14:21:30 +0100

    feat(cli): add cli argument parsing

--- EasySave/Program.cs
+++ EasySave/Program.cs
@@ -5,16 +5,48 @@ using EasySave.ViewModel;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         var stateTracker = new StateTracker();
         var manager = new BackupManager(stateTracker);
 
-        var consoleProgress = new ConsoleProgressDisplay(); 
-        //stateTracker.Attach(consoleProgress); 
-        
+        var consoleProgress = new ConsoleProgressDisplay();
+        //stateTracker.Attach(consoleProgress);
 
-        var view = new ConsoleView();
-        view.DisplayMenu();
+
+        // TODO: handle args for CLI mode
+        if (args.Length > 0)
+        {
+            ExecuteFromArgs(args, manager);
+        }
+        else
+        {
+            var view = new ConsoleView();
+            view.DisplayMenu();
+        }
+    }
+
+    //executes the backup from args
+    static void ExecuteFromArgs(string[] args, BackupManager manager)
+    {
+        // TODO: need to check if jobs exist first
+
+        string input = args[0];
+
+        // trying to parse range
+        if (input.Contains("-"))
+        {
+            // split
+            string[] parts = input.Split("-");
+            int start = int.Parse(parts[0]);
+            int end = int.Parse(parts[1]);
+
+            manager.ExecuteRange(start, end);
+        }
+        else
+        {
+            int index = int.Parse(input);
+            manager.ExecuteJob(index);
+        }
     }
 }
