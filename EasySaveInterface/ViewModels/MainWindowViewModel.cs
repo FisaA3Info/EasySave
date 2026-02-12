16be943d11a9f9a846f4ce2294f3faa34d154180
Floraaa <150044772+LonelyBadeline@users.noreply.github.com>
2026-02-12 09:45:39 +0100

    feat(View): Adding Avalonia.ReactiveUI to the project + implementing short header and dropdown list for both languages and log format

--- EasySaveInterface/ViewModels/MainWindowViewModel.cs
+++ EasySaveInterface/ViewModels/MainWindowViewModel.cs
@@ -1,32 +1,53 @@
-﻿namespace EasySaveInterface.ViewModels
+﻿using HarfBuzzSharp;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace EasySaveInterface.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
 
         public string Title { get; set; } = "EasySave";
-        public void LaunchBackupCommand()
-        {
+        private string selectedLanguage = "Français";
+        private string selectedLogFormat = "JSON";
 
+        public ObservableCollection<string> Languages { get; }
+        public ObservableCollection<string> LogFormats { get; }
+
+        public string SelectedLanguage
+        {
+            get => selectedLanguage;
+            set => SetProperty(ref selectedLanguage, value);
         }
 
-        public void ShowOngoingBackupsCommand()
+        public string SelectedLogFormat
         {
-
+            get => selectedLogFormat;
+            set => SetProperty(ref selectedLogFormat, value);
         }
 
-        public void ShowFinishedBackupsCommand()
+        // Commandes
+        public ICommand CreateJobCommand { get; }
+        public ICommand ExecuteJobCommand { get; }
+        public ICommand ExecuteAllJobsCommand { get; }
+        public ICommand ExecuteRangeCommand { get; }
+        public ICommand ExecuteSelectedJobsCommand { get; }
+        public ICommand DeleteJobCommand { get; }
+        public ICommand ShowAllJobsCommand { get; }
+
+        public MainWindowViewModel()
         {
-
-        }
-
-        public void ShowErrorsCommand()
-        {
-
-        }
-
-        public void ShowSettingsCommand()
-        {
-
+            Languages = new ObservableCollection<string> { "Français", "English" };
+            LogFormats = new ObservableCollection<string> { "JSON", "XML" };
+            // Command Init 
+            CreateJobCommand = ReactiveCommand.Create(() => { /* Create Job toussatoussa */ });
+            ExecuteJobCommand = ReactiveCommand.Create(() => { /* Exec Job blabla */ });
+            ExecuteAllJobsCommand = ReactiveCommand.Create(() => { /* Exec all job braaaaaaaaaw*/ });
+            ExecuteRangeCommand = ReactiveCommand.Create(() => { /* Exec job range go brrrrr*/ });
+            ExecuteSelectedJobsCommand = ReactiveCommand.Create(() => { /* Exec specific job yum yum */ });
+            DeleteJobCommand = ReactiveCommand.Create(() => { /* Delete job hmmmm */ });
+            ShowAllJobsCommand = ReactiveCommand.Create(() => { /* Show All job*/ });
         }
 
 
